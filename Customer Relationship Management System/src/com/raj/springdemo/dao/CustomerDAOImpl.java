aea4a7efa4e7ce38aeffb495ac1eb5b99045ca53
VenkateswaraRajuDatla <35017300+VenkateswaraRajuDatla@users.noreply.github.com>
2019-02-27 21:21:39 -0800

    Update CustomerDAOImpl.java

--- Customer Relationship Management System/src/com/raj/springdemo/dao/CustomerDAOImpl.java
+++ Customer Relationship Management System/src/com/raj/springdemo/dao/CustomerDAOImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.luv2code.springdemo.entity.Customer;
+import com.raj.springdemo.entity.Customer;
 
 @Repository
 public class CustomerDAOImpl implements CustomerDAO {
